08b785d30484fc2221335dcfff51682e1411f074
Maya <168336946+Mayaemx@users.noreply.github.com>
2024-06-27 16:55:34 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,16 +10,16 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://telegra.ph/file/63d3159644e518947e654.jpg",
+    "https://telegra.ph/file/c7749599d0e4bf2071569.jpg",
+    "https://telegra.ph/file/1e06b91bd52ec768de2bb.jpg",
+    "https://telegra.ph/file/460f91ba860f2e6bf9144.jpg",
+    "https://telegra.ph/file/9c28cddd0df594fcd6b8a.jpg",
+    "https://telegra.ph/file/52475013ea9e235eb725e.jpg",
+    "https://telegra.ph/file/43f7d43e5ad5a5601f093.jpg",
 ]
 
-HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
+HEY_IMG = "https://telegra.ph/file/c3616f6a49f181e9a210f.jpg"
 
 ALIVE_ANIMATION = [
     "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
