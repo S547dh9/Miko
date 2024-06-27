7d8a383059e25d545cf5deaa250d5730bc42ab78
Maya <168336946+Mayaemx@users.noreply.github.com>
2024-06-27 18:05:36 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -34,9 +34,9 @@ ALIVE_ANIMATION = [
 
 FIRST_PART_TEXT = "🪷 *ʜᴇʏ* `{}` . . ."
 
-PM_START_TEXT = "🫧 *ɪ ᴀᴍ ᴍɪᴋᴏ 🫧, ⏣ Here, you will find a list of all the available commands.
+PM_START_TEXT = "🫧 *ɪ ᴀᴍ ᴍɪᴋᴏ 🫧, ⏣ Here, you will find a list of all the available commands
 
-ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : / .*"
+ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /*"
 
 START_BTN = [
     [
