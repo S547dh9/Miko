35e75b48a572f655edf69d10a46b54bd2feb8e3b
Maya <168336946+Mayaemx@users.noreply.github.com>
2024-06-28 18:51:16 +0530

    Update tagall.py

--- Mikobot/plugins/tagall.py
+++ Mikobot/plugins/tagall.py
@@ -5,7 +5,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from MukeshRobot import telethn as client
+from Mikobot import telethn as client
 
 spam_chats = []
 
